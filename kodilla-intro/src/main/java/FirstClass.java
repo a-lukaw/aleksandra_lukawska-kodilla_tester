f39e4d6d253513c436ae5f61caade6c1c8b8897e
Aleksandra Łukawska <lukawska.aleksandra1@gmail.com>
2023-02-11 18:59:44 +0100

    cwiczenie instrukcje warunkowe

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,9 +1,15 @@
 public class FirstClass {
     public static void main(String[] args) {
-        if (1 > 2) {
-            System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
-        } else {
-            System.out.println("Wszystko po staremu, jeden jest mniejsze niż dwa.");
-        }
+        Notebook notebook = new Notebook(600, 1500, 2020);
+        System.out.println(notebook.weight + " " + notebook.price + " " + notebook.year);
+        notebook.checkPrice(); notebook.checkWeight(); notebook.checkYearAndPrice();
+
+        Notebook heavyNotebook = new Notebook(1200, 1000, 2015);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price + " " + notebook.year);
+        heavyNotebook.checkPrice(); heavyNotebook.checkWeight(); notebook.checkYearAndPrice();
+
+        Notebook oldNotebook = new Notebook(2000, 500, 2010);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price + " " + notebook.year);
+        oldNotebook.checkPrice(); oldNotebook.checkWeight(); notebook.checkYearAndPrice();
     }
 }
