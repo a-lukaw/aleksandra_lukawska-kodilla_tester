7a3ef2914c22ecfbf422287f031f0ae7962971ab
Aleksandra Łukawska <lukawska.aleksandra1@gmail.com>
2023-03-14 15:18:16 +0100

    'zadanie 2.7. Zmienne i pola statyczne'

---
+++ kodilla-intro/src/main/java/com/Book.java
@@ -0,0 +1,31 @@
+package com;
+
+public class Book {
+
+    //tworzymy pola klasy
+    private String author;
+
+    private String title;
+
+    //tworzymy konstruktor [Alt + insert]
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    /*metoda statyczna of, któa jako parametr przyjmuje autora i tytuł książki
+    metoda ta ma zwracać utworzony obiekt książki*/
+    public static Book of(String author, String title) {
+        return new Book(author, title);
+    }
+
+    //przykładowa baza danych, z której wyżej wyciągamy interesujące nas dane
+    public static void main(String[] args) {
+        Book book  = Book.of("Isaac Asimov", "The Galaxy");
+        Book bookX = new Book("Isaac Asimov", "The Galaxy");
+        Book book1 = Book.of("Isaac Asimov", "The Galaxy");
+        Book book2 = Book.of("Isaac Asimov", "The Galaxy");
+        Book book3 = Book.of("Isaac Asimov", "The Galaxy");
+        Book book4 = Book.of("ala ma kota", "kodilla");
+    }
+}
