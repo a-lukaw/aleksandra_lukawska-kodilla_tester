375889f1380957d42c48c1ee62cdafa6606f031f
Aleksandra Łukawska <lukawska.aleksandra1@gmail.com>
2023-03-14 15:14:46 +0100

    'zadanie 2.5. Pętle'

---
+++ kodilla-intro/src/main/java/com/RandomNumbers.java
@@ -0,0 +1,40 @@
+package com;
+
+import java.util.Random;
+
+public class RandomNumbers {
+    int sum;
+    int min;
+    int max;
+
+    public RandomNumbers() {
+        this.sum = 0;
+        this.min = 31;
+        this.max = 0;
+    }
+
+    public int getCountOfRandomNumbers() {
+
+        //utworzenie obiektu typu Random
+
+        Random random = new Random();
+        int temp;
+        while (this.sum < 5000) {
+            temp = random.nextInt(31);
+            sum = sum + temp;
+            if (this.min > temp) {
+                min = temp;
+            }
+            if (this.max < temp) {
+                max = temp;
+            }
+        }
+    return sum;
+    }
+    public int getMin() {
+        return this.min;
+    }
+    public int getMax() {
+        return this.max;
+    }
+}
---
+++ kodilla-intro/src/main/java/com/User.java
@@ -0,0 +1,48 @@
+package com;
+
+public class User {
+    //nadajemy klasie atrybuty (zmienne)//
+    String userName;
+    int userAge;
+
+    public User(String userName, int userAge) {
+        this.userName = userName;
+        this.userAge = userAge;
+    }
+
+    /*tworzymy obiekty, którymi chcemy wypełnić tablicę
+    każdy obiekt ma dwie właściwości: imię oraz wiek*/
+
+    public static void main(String[] args) {
+        User ola = new User("Ola", 34);
+        User ania = new User("Ania", 28);
+        User marta = new User("Marta", 35);
+        User ewelcia = new User("Ewelcia", 34);
+        User arkadiusz = new User("Arkadiusz", 43);
+        User hubert = new User("Hubert", 45);
+        User michal = new User("Michal", 35);
+        User felek = new User("Felek", 2);
+        User ewa = new User("Ewa", 68);
+        User wojciech = new User("Wojciech", 70);
+
+        //tworzymy tablicę o nazwie User i wypełniamy ją obiektami stworzonymi powyżej//
+        User[] users = {ola, ania, marta, ewelcia, arkadiusz, hubert, michal, felek, ewa, wojciech};
+
+        //za pomocą pętli for wyciągamy z każdego obiektu typu User wartość pola, w którym zapisany jest wiek//
+
+        double sumAge = 0;
+        for (int i = 0; users.length > i; i++) {
+            sumAge = sumAge + users[i].userAge;
+        }
+        double avgAge = sumAge / users.length;
+
+        for (int i = 0; users.length > i; i++) {
+            int Age = users[i].userAge;
+
+            //wyświetlenie użytkowników, których wiek < średnia//
+            if(Age < avgAge) {
+                System.out.println(users[i].userName);
+            }
+        }
+    }
+}
