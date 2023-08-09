dd41c6778a4bd1bf1df5bce8fb2f4b1bad5bb049
Aleksandra Łukawska <lukawska.aleksandra1@gmail.com>
2023-08-09 15:30:31 +0200

    'Zadanie 2.4. Tablice'

--- kodilla-intro/src/main/java/comm/Grades.java
+++ kodilla-intro/src/main/java/comm/Grades.java
@@ -1,35 +1,26 @@
 package comm;
 
+//Zad. 2.3. utwórz klasę Grades, która będzie przechowywać 10 ocen ucznia. Klasa powinna zawierać 3 metody:
+//dodającą przekazaną w argumencie ocenę do tablicy,
+//zwracającą ostatnio dodaną ocenę,
+//zwracającą średnią ocen.
+
 public class Grades {
-    //dodajemy tablicę, która będzie przechowywać 10 wartości (ocen)
     private int[] grades;
 
-    //Dodajemy możliwość wstawienia nowej oceny do zbioru. Aby dodać element do tablicy, musimy wskazać,
-    // na której pozycji powinien on się znaleźć.
-    // W tym celu możemy utworzyć dodatkową zmienną, która będzie "monitorować"
-    // aktualną liczbę elementów w tablicy.
-    //Zmienna size mówi nam, ile elementów aktualnie znajduje się w tablicy.
-    // Jej wartość powinna być modyfikowana, gdy zmieniamy wartości w tablicy.
-    private int size;
+    private int lastIndex;
 
-    //wywołujemy kontruktor
-    public Grades() {
-
-        //przypisujemy nową wartość do zmiennej, przechowującej oceny//
-        this.grades = new int[10];
-        this.size = 0;
+    public Grades() {                                                                           //wywołujemy kontruktor
+        this.grades = new int[10];                                                             //przypisujemy nową wartość do zmiennej, przechowującej oceny//
+        lastIndex = -1;
     }
 
     //metoda dodająca przekazaną w argumencie ocenę do tablicy
-    //Jeśli nasza zmienna odpowiadająca za przechowywanie rozmiaru tablicy
-    // jest równa 10 (długość tablicy), przerywamy działanie metody
-    public void add(int value) {
-        if (this.size == 10) {
-            return;
+    public void addGrade(int grade) {
+        if (lastIndex < 9) {
+            lastIndex++;
+            grades[lastIndex] = grade;
         }
-        //dodanie elementu do tablicy, jeśli rozmiar jest mniejszy od maksymalnego
-        this.grades[this.size] = value;
-        this.size++;
     }
 
     //metoda sumująca oceny z tablicy
@@ -40,12 +31,26 @@ public class Grades {
         }
         return sumGrades;
     }
-     double averageGrades() {
-         int sumGrades = 0;
-         for (int i = 0; this.grades.length > i; i++) {
-             sumGrades = sumGrades + this.grades[i];
-         }
-         return sumGrades / this.grades.length;
-     }
 
- }
+    //metoda zwracająca ostatnio dodaną ocenę
+    public int getLastAddedGrade() {
+        if (lastIndex >= 0) {
+            return grades[lastIndex];
+        } else {
+            return -1;
+        }
+    }
+
+    // metoda zwracająca średnią ocen
+    public double calculateAverage() {
+        if (lastIndex >= 0) {
+            int sum = 0;
+            for (int i = 0; i <= lastIndex; i++) {
+                sum += grades[i];
+            }
+            return (double) sum / (lastIndex + 1);
+        } else {
+            return 0.0;                                                     // Specjalna wartość, wskazująca brak ocen
+        }
+    }
+}
--- kodilla-intro/src/main/java/comm/GradesMain.java
+++ kodilla-intro/src/main/java/comm/GradesMain.java
@@ -1,11 +1,17 @@
 package comm;
 
 public class GradesMain {
-    public static void main(String[] args) {
-        Grades g = new Grades();
-        g.add(5);
-        g.add(2);
-        int sum = g.sumGrades();
-        System.out.println(sum);
+        public static void main(String[] args) {
+            Grades studentGrades = new Grades();
+
+            studentGrades.addGrade(5);
+            studentGrades.addGrade(2);
+            studentGrades.addGrade(3);
+
+            int lastAddedGrade = studentGrades.getLastAddedGrade();
+            System.out.println("Ostatnio dodana ocena: " + lastAddedGrade);
+
+            double averageGrade = studentGrades.calculateAverage();
+            System.out.println("Średnia ocen wynosi: " + averageGrade);
+        }
     }
-}
