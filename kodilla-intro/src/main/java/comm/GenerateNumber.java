345967dfd4d55a4a436fa569c9d2c1a0fcf88d06
Aleksandra Łukawska <lukawska.aleksandra1@gmail.com>
2023-08-10 11:31:01 +0200

    'Zadanie 2.5. Pętle - losowanie liczb'

--- kodilla-intro/src/main/java/comm/GenerateNumber.java
+++ kodilla-intro/src/main/java/comm/GenerateNumber.java
@@ -1,10 +1,13 @@
 package comm;
 public class GenerateNumber {
     public static void main(String[] args) {
-        RandomNumbers generator = new RandomNumbers();
-        generator.getCountOfRandomNumbers();
-        int minValue = generator.getMin();
+        RandomNumbers generator = new RandomNumbers();                   //Tworzy obiekt klasy RandomNumbers o nazwie generator przy użyciu konstruktora bezparametrowego, pozwala na korzystanie z metod i właściwości zdefiniowanych w klasie RandomNumbers
+
+        generator.getCountOfRandomNumbers();                              //Wywołuje metodę generateRandomNumbers() na obiekcie generator
+
+        int minValue = generator.getMin();                                //Wywołuje metodę getMin() na obiekcie generator, aby uzyskać minimalną wylosowaną wartość, a następnie przypisuje ją do zmiennej minValue
         int maxValue = generator.getMax();
+
         System.out.println(minValue);
         System.out.println(maxValue);
     }
--- kodilla-intro/src/main/java/comm/RandomNumbers.java
+++ kodilla-intro/src/main/java/comm/RandomNumbers.java
@@ -1,39 +1,44 @@
 package comm;
 
+//Utwórz klasę RandomNumbers, w której będzie istnieć metoda losująca liczby z zakresu 0-30, tak długo, aż ich suma przekroczy 5000.
+//Napisz dwie metody zwracające największą i najmniejszą wylosowaną wartość
+
 import java.util.Random;
 
 public class RandomNumbers {
-    int sum;
-    int min;
-    int max;
+    int min;                                                                            //minimalna z wylosowanych liczb
+    int max;                                                                          //maksymalna z wyolosowanych liczb
+    int sum;                                                                                       //suma losowych liczb
 
     public RandomNumbers() {
+        this.min = 0;
+        this.max = 31;
         this.sum = 0;
-        this.min = 31;
-        this.max = 0;
     }
 
-    public int getCountOfRandomNumbers() {
+    public void getCountOfRandomNumbers() {
 
-        //utworzenie obiektu typu Random
+        Random random = new Random();                                                   //utworzenie obiektu typu random
 
-        Random random = new Random();
-        int temp;
+        int temp;                                                                  //utworzenie zmiennej temp, która będzie przechowywać tymczasową sumę liczb
+
         while (this.sum < 5000) {
             temp = random.nextInt(31);
-            sum = sum + temp;
+            this.sum += temp;
+
             if (this.min > temp) {
-                min = temp;
+                this.min = temp;
             }
             if (this.max < temp) {
-                max = temp;
+                this.max = temp;
             }
         }
-    return sum;
     }
+
     public int getMin() {
         return this.min;
     }
+
     public int getMax() {
         return this.max;
     }
